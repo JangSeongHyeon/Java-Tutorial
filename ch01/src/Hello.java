a08058e5f92c3177d06b049450baeef683d4f242
JangSeongHyeon <dwg04045@gmail.com>
2020-12-18 08:50:33 +0900

    Initialize Java Setup and Text Output

---
+++ ch01/src/Hello.java
@@ -0,0 +1,21 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl+shift+l 단축키 전체 목록 보기
+		// ctrl+ + or - 소스코드 키우기
+		// ctrl+D 한 줄 삭제
+		// ctrl+alt+down 행단위 복사
+		// alt+shift+A 멀티 컬림편집
+		// Window메뉴 > Preferences > Keys에 가면 단축키 수정가능
+		// alt+ up, down 행단위 이동
+		// 들여쓰기 tab, 내어쓰기 shit+tab
+		// ctrl+i 자동으로 들여쓰기
+		// ctrl+/ 주석처리
+		// ctrl+space 자동완성
+		// Window메뉴 > Preferences > Java> Editors > Templates에 가면 자동완성 기능 수정가능
+	
+		System.out.println("Hello,java");
+	}
+}   
